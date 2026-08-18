31b14166505665e1bf469387a36956da7a8a723a
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-18 00:47:59 -0400

    Add stone-game-v (python3) - LeetCode solution

--- solutions/stone-game-v.py
+++ solutions/stone-game-v.py
@@ -39,7 +39,7 @@ class Solution:
                 if lpos[l][r] >= l:
                     best = max(best, lmax[l][lpos[l][r]])
                 else:
-                    best = max(best, rmax[rpos[l][r]][r])
+                    best = max(best, rmax[rpos[l][r]+1][r])
                 dp[l][r] = best
                 tot = pref[r+1] - pref[l]
                 lmax[l][r] = max(lmax[l][r-1], tot + dp[l][r])
