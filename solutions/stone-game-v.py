5ac00a56840973751610cb0ae68b8ae260ff4b39
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-18 00:46:20 -0400

    Add stone-game-v (python3) - LeetCode solution

--- solutions/stone-game-v.py
+++ solutions/stone-game-v.py
@@ -1,83 +1,49 @@
 class Solution:
     def stoneGameV(self, stoneValue: List[int]) -> int:
         n = len(stoneValue)
-
-        pref = [0] * (n + 1)
-        for i in range(n):
-            pref[i + 1] = pref[i] + stoneValue[i]
-
+        pref = [0] + list(accumulate(stoneValue))
+        dp = [[0] * n for _ in range(n)]
+        lmax = [[0] * n for _ in range(n)]
+        rmax = [[0] * n for _ in range(n)]
         lpos = [[-1] * n for _ in range(n)]
         rpos = [[-1] * n for _ in range(n)]
-
-        # Precompute boundaries
         for l in range(n):
             lp = l - 1
             rp = l
+            for r in range(l+1, n):
+                tot = pref[r+1] - pref[l]
 
-            for r in range(l + 1, n):
-                total = pref[r + 1] - pref[l]
-
-                # Last k with left <= right
-                while lp + 1 <= r - 1:
-                    k = lp + 1
-                    left_sum = pref[k + 1] - pref[l]
-
-                    if 2 * left_sum > total:
+                while lp + 1 < r:
+                    lsum = pref[lp+2] - pref[l]
+                    if lsum * 2 > tot:
                         break
-
                     lp += 1
-
-                # First k with left >= right
-                while rp <= r - 1:
-                    k = rp
-                    left_sum = pref[k + 1] - pref[l]
-
-                    if 2 * left_sum >= total:
-                        break
-
-                    rp += 1
-
                 lpos[l][r] = lp
+
+                while rp < r:
+                    lsum = pref[rp+1] - pref[l]
+                    if lsum * 2 > tot:
+                        break
+                    rp += 1
                 rpos[l][r] = rp
 
-        dp = [[0] * n for _ in range(n)]
-
-        left_best = [[0] * n for _ in range(n)]
-        right_best = [[0] * n for _ in range(n)]
-
         for i in range(n):
-            left_best[i][i] = stoneValue[i]
-            right_best[i][i] = stoneValue[i]
+            lmax[i][i] = stoneValue[i]
+            rmax[i][i] = stoneValue[i]
 
-        for length in range(2, n + 1):
-            for l in range(n - length + 1):
+        for length in range(2, n+1):
+            for l in range(0, n - length + 1):
                 r = l + length - 1
-
+                res = 0
                 best = 0
-
-                # left <= right
                 if lpos[l][r] >= l:
-                    best = left_best[l][lpos[l][r]]
-
-                # left >= right
-                if rpos[l][r] <= r - 1:
-                    best = max(
-                        best,
-                        right_best[rpos[l][r] + 1][r]
-                    )
-
+                    best = max(best, lmax[l][lpos[l][r]])
+                else:
+                    best = max(best, rmax[rpos[l][r]][r])
                 dp[l][r] = best
-
-                total = pref[r + 1] - pref[l]
-
-                left_best[l][r] = max(
-                    left_best[l][r - 1],
-                    dp[l][r] + total
-                )
-
-                right_best[l][r] = max(
-                    right_best[l + 1][r],
-                    dp[l][r] + total
-                )
-
-        return dp[0][n - 1]
+                tot = pref[r+1] - pref[l]
+                lmax[l][r] = max(lmax[l-1][r], tot + dp[l][r])
+                rmax[l][r] = max(rmax[l][r-1], tot + dp[l][r])
+                
+        return dp[0][n-1]
+        
