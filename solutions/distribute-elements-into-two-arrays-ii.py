467118a4b8d386e0a0b90e2495df70c14e92332b
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-19 15:51:40 -0400

    Add distribute-elements-into-two-arrays-ii (python3) - LeetCode solution

--- solutions/distribute-elements-into-two-arrays-ii.py
+++ solutions/distribute-elements-into-two-arrays-ii.py
@@ -1,7 +1,9 @@
 class Solution:
     def resultArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        res = []
+        a1 = []
+        a2 = []
+
         ssl = SortedList([nums[0]])
         ssr = SortedList([nums[1]])
         
@@ -9,12 +11,16 @@ class Solution:
             l = len(ssl) - ssl.bisect_right(nums[i])
             r = len(ssr) - ssr.bisect_right(nums[i])
             if l > r:
+                a1.append(nums[i])
                 ssl.add(nums[i])
             elif l < r:
+                a2.append(nums[i])
                 ssr.add(nums[i])
             else:
                 ta = ssl if len(ssl) <= len(ssr) else ssr
+                tb = a1 if len(ssl) <= len(ssr) else a2
                 ta.add(nums[i])
-        print(ssl, ssr)
-        return list(ssl) + list(ssr)
+                tb.append(nums[i])
+        # print(ssl, ssr)
+        return a1 + a2
 
