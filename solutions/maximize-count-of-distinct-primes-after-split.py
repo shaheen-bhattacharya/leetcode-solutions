7576d0874888007c16a26e831406bac3339c73fd
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-20 12:09:55 -0400

    Add maximize-count-of-distinct-primes-after-split (python3) - LeetCode solution

---
+++ solutions/maximize-count-of-distinct-primes-after-split.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumCount(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        def sieve(n):
+            lp = [0] * (n+1)
+            primes = []
+            for i in range(2, n+1):
+                if lp[i] == 0:
+                    lp[i] = i
+                    primes.append(i)
+                for p in primes:
+                    if p * i > n or p > lp[i * p]:
+                        break
+                    lp[p*i] = p
+            return lp, primes
+
+        maxv = max(nums)
+        n = len(nums)
+        lp, primes = sieve(maxv)
+        print(lp)
+
+        # @cache
+        # def isPrime(num):
+
+
+        # tree = [0] * (4 * n) 
+        # def build(node, l, r):
