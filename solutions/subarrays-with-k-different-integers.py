0736faf74aa9ab44c628d4921fe3a442579ed96c
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-20 11:02:21 -0400

    Add subarrays-with-k-different-integers (python3) - LeetCode solution

--- solutions/subarrays-with-k-different-integers.py
+++ solutions/subarrays-with-k-different-integers.py
@@ -4,26 +4,30 @@ class Solution:
         l = 0
         res = 0
         freq = defaultdict(int)
+        lidx = {}
+        sl = SortedList()
         for r in range(n):
             freq[nums[r]] += 1
-            print(freq)
+            if nums[r] not in lidx:
+                sl.add(r)
+            else:
+                sl.discard(lidx[nums[r]])
+                sl.add(r)
+            lidx[nums[r]] = r
+
             curr = len(freq)
             while l <= r and curr > k:
                 freq[nums[l]] -= 1
                 if freq[nums[l]] == 0:
                     del freq[nums[l]]
+                    sl.discard(lidx[nums[l]])
                     curr -= 1
                 l += 1
 
             tmp = defaultdict(int)
             tl = l
-            while curr == k and tl <= r:
-                # print("d")
-                res += 1
-                tmp[nums[tl]] += 1
-                if tmp[nums[tl]] == freq[nums[tl]]:
-                    break
-                tl += 1
+            i
+            res += sl[0] - l
         return res
             
                 
