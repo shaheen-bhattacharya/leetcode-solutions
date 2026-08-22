bb0b9ad67f19c698a33b08e801005b1680a3a0eb
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-22 11:01:56 -0400

    Add reconstruct-itinerary (python3) - LeetCode solution

---
+++ solutions/reconstruct-itinerary.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(set)
+        n = len(tickets)
+        for u, v in tickets:
+            adj[u].add(v)
+
+        res = []
+        def dfs(node, adj, curr):
+            nonlocal res
+            if len(curr) == n+1:
+                if res == []:
+                    res = curr
+                else:
+                    res = min(res, curr)
+                return 
+            for nei in adj[node]:
+                adj[node].remove(nei)
+                curr.append(nei)
+                adj[node].add(nei)
+        dfs("JFK", adj, [])
+        return res
