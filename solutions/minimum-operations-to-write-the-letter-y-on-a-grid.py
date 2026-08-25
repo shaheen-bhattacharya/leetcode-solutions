8da3ab0f0b531496a7aebea7638bc878ff892261
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-25 17:30:25 -0400

    Add minimum-operations-to-write-the-letter-y-on-a-grid (python3) - LeetCode solution

---
+++ solutions/minimum-operations-to-write-the-letter-y-on-a-grid.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minimumOperationsToWriteY(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        fin = defaultdict(int)
+        fout = defaultdict(int)
+
+        for r in range(rows//2):
+            for c in range(cols//2):
+                if r == c:
+                    fin[grid[r][c]] += 1
+                else:
+                    fout[grid[r][c]] += 1
+
+        for r in range(rows//2):
+            for c in range(cols-1, cols//2, -1):
+                if cols - c - 1 == r:
+                    fin[grid[r][c]] += 1
+                else:
+                    fout[grid[r][c]] += 1
+
+        for r in range(rows//2, rows):
+            for c in range(cols):
+                if r == rows//2:
+                    fin[grid[r][c]] += 1
+                else:
+                    fout[grid[r][c]] += 1
+        res = inf
+        res = min(res, fin[0] + fin[2] + fout[1] + fout[2], fin[0] + fin[2] + fout[0] + fout[1])
+        res = min(res, fin[1] + fin[2] + fout[1] + fout[0], fin[1] + fin[2] + fout[0] + fout[2])
+        res = min(res, fin[0] + fin[1] + fout[0] + fout[2], fin[0] + fin[1] + fout[1] + fout[2])
+        return res
+
+
