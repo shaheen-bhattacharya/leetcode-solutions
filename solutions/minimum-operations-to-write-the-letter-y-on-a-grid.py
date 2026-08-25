00c59bd69e18444cbaebecffff99dcba89f68ebb
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-25 17:35:21 -0400

    Add minimum-operations-to-write-the-letter-y-on-a-grid (python3) - LeetCode solution

--- solutions/minimum-operations-to-write-the-letter-y-on-a-grid.py
+++ solutions/minimum-operations-to-write-the-letter-y-on-a-grid.py
@@ -22,7 +22,7 @@ class Solution:
         for r in range(rows//2, rows):
             for c in range(cols):
                 if r == rows//2:
-                    print(r, c)
+                    print("a",r, c)
                     fin[grid[r][c]] += 1
                 else:
                     fout[grid[r][c]] += 1
