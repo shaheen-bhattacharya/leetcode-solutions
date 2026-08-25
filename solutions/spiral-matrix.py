4a8372a8a629849b8405de2d123f11af8af1407e
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-25 17:08:14 -0400

    Add spiral-matrix (python3) - LeetCode solution

--- solutions/spiral-matrix.py
+++ solutions/spiral-matrix.py
@@ -8,10 +8,10 @@ class Solution:
                 res.append(matrix[i][c])
             for r in range(i+1, rows-i-1):
                 res.append(matrix[r][-i-1])
-            if cols//2 != i:
+            if rows//2 != i:
                 for c in range(cols-i-1, i-1, -1):
                     res.append(matrix[-i-1][c])
-            if rows//2 != i:
+            if cols//2 != i:
                 for r in range(rows-i-2, i, -1):
                     res.append(-matrix[r][i])
         return res
