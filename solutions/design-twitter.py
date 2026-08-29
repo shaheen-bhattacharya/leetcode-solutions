e1c7d00062fed2136377ef4076e986cdd067ce73
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-29 11:48:03 -0400

    Add design-twitter (python3) - LeetCode solution

--- solutions/design-twitter.py
+++ solutions/design-twitter.py
@@ -14,30 +14,39 @@ class Twitter:
 
     def getNewsFeed(self, userId: int) -> List[int]:
         heap = []
+
+        # Include the user's own tweets
         users = self.following[userId] | {userId}
 
+        # Add newest tweet from each user
         for user in users:
             if self.tweets[user]:
                 i = len(self.tweets[user]) - 1
                 time, tweetId = self.tweets[user][i]
 
-                heapq.heappush(heap, (time, tweetId, user, i - 1))
+                # Negative because heapq is a min-heap
+                heapq.heappush(
+                    heap,
+                    (-time, tweetId, user, i - 1)
+                )
 
         res = []
 
+        # K-way merge, stopping after 10 tweets
         while heap and len(res) < 10:
-            time, tweetId, user, i = heapq.heappop(heap)
+            neg_time, tweetId, user, i = heapq.heappop(heap)
             res.append(tweetId)
 
+            # Add next-oldest tweet from this user
             if i >= 0:
                 time, tweetId = self.tweets[user][i]
 
                 heapq.heappush(
                     heap,
-                    (time, tweetId, user, i - 1)
+                    (-time, tweetId, user, i - 1)
                 )
 
-        return res[::-1]
+        return res
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.following[followerId].add(followeeId)
