6ef73c89cc04ed2c58fde902bcf9dc983ab9f644
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-30 15:40:01 -0400

    Add equal-sum-grid-partition-ii (python3) - LeetCode solution

---
+++ solutions/equal-sum-grid-partition-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canPartitionGrid(self, grid: List[List[int]]) -> bool:
+        rows, cols = len(grid), len(grid[0])
+        dp = [0]*cols
+
+        for r in range(rows):
+            ndp = [0]*cols
+            pref = [0] + list(accumulate(grid[r]))
+            tot = pref[-1]
+            for c in range(cols):
+                ndp[c] += dp[c] + tot - 2 * pref[c+1]
+                if r == rows-1 and ndp[c] == 0:
+                    return True
+            dp = ndp
+        return False
