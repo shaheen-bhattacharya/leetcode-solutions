cd93a3af233e6b374037de32920d6296785f0e93
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-29 20:54:05 -0400

    Add removing-minimum-and-maximum-from-array (python3) - LeetCode solution

--- solutions/removing-minimum-and-maximum-from-array.py
+++ solutions/removing-minimum-and-maximum-from-array.py
@@ -3,15 +3,15 @@ class Solution:
         mindx = 0
         maxdx = 0
         n = len(nums)
-        # if n == 1:
-        #     return 0
+        if n == 1:
+            return 1
         for i in range(1, n):
             if nums[i] > nums[maxdx]:
                 maxdx = i
             if nums[i] < nums[mindx]:
                 mindx = i
         i, j = min(mindx, maxdx), max(mindx, maxdx)
-        return min(i+1+n-j, j+1, n-i+1)
+        return min(i+1+n-j, j+1, n-i)
         print(i+n-j-1)
     
             
