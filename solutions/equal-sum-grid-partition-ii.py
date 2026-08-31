ee942e6043e76597432db9e1e6a315f54d1241b0
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-08-30 21:44:35 -0400

    Add equal-sum-grid-partition-ii (python3) - LeetCode solution

--- solutions/equal-sum-grid-partition-ii.py
+++ solutions/equal-sum-grid-partition-ii.py
@@ -15,22 +15,31 @@ class Solution:
 
         for r in range(rows-1):
             rs = 0
+            cb = set()
+            ct = set()
             for c in range(cols):
                 top[grid[r][c]] += 1
                 bottom[grid[r][c]] -= 1
                 rs += grid[r][c]  
             tsum += rs
             bsum -= rs
-            print(tsum, bsum)
             if bsum == tsum:
                 return True
             diff = max(bsum, tsum) - min(bsum, tsum)
             if bsum > tsum:
-                if bottom[diff] > 0:
-                    return True
+                if r == rows-2:
+                    if (grid[r][-1] == diff or grid[r][0] == diff):
+                        return True
+                else:
+                    if bottom[diff] > 0:
+                        return True
             if bsum < tsum:
-                if top[diff] > 0:
-                    return True
+                if r == 0:
+                    if (grid[r][0] == diff or grid[r][-1] == diff):
+                        return True
+                else:
+                    if top[diff] > 0:
+                        return True
         return False
             
 
