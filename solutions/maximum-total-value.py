8bd9f08d4e0d702ef8dbd6835a8ef0193bca042d
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-03 00:45:16 -0400

    Add maximum-total-value (python3) - LeetCode solution

--- solutions/maximum-total-value.py
+++ solutions/maximum-total-value.py
@@ -2,33 +2,40 @@ class Solution:
     def maxTotalValue(self, value: list[int], decay: list[int], m: int) -> int:
         MOD = 1_000_000_007
         n = len(value)
+
         def good(th):
             tot = 0
             ops = 0
+
             for i in range(n):
                 if value[i] < th:
                     continue
-                need = (value[i] - th)//decay[i] + 1
+
+                need = (value[i] - th) // decay[i] + 1
+
                 if need > 0:
-                    tot += value[i] * need - decay[i] * (need - 1) * need//2
+                    tot += (
+                        value[i] * need
+                        - decay[i] * (need - 1) * need // 2
+                    )
                     ops += need
-            return (tot, ops)
-        
+
+            return tot, ops
+
         maxv = max(value)
+
         l, r = 0, maxv + 1
+
         while l < r:
-            mid = (l+r)//2
+            mid = (l + r) // 2
+
             if good(mid)[1] > m:
                 l = mid + 1
             else:
                 r = mid
 
         t, o = good(l)
-        t += (m-o) * (l-1)
+
+        t += (m - o) * (l - 1)
+
         return t % MOD
-
-
-
-                
-
-            
