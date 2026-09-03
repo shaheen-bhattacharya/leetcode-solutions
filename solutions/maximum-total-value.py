642e6bb175e392050740b0d27ce93bcf3efe3567
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-03 00:27:59 -0400

    Add maximum-total-value (python3) - LeetCode solution

--- solutions/maximum-total-value.py
+++ solutions/maximum-total-value.py
@@ -18,7 +18,7 @@ class Solution:
         l, r = 0, maxv + 1
         while l < r:
             mid = (l+r)//2
-            if good(l)[0] < good(mid)[0]:
+            if good(mid)[1] < m:
                 l = mid + 1
             else:
                 r = mid
