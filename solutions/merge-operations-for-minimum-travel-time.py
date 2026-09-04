dd273dc472291d9409059b3aac4d6d3c0840c4db
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-03 22:26:13 -0400

    Add merge-operations-for-minimum-travel-time (python3) - LeetCode solution

--- solutions/merge-operations-for-minimum-travel-time.py
+++ solutions/merge-operations-for-minimum-travel-time.py
@@ -15,7 +15,7 @@ class Solution:
             if amt > k:
                 return inf
             
-            rate = pref[i+1] - pref[pi+1]
+            rate = pref[i+1] - pref[pi]
             ret = inf
             t = 0
             for j in range(i+1, n):
