da4d189f29bdaac4facc400ca9b6f5a3be22b7d7
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-03 21:59:38 -0400

    Add merge-operations-for-minimum-travel-time (python3) - LeetCode solution

--- solutions/merge-operations-for-minimum-travel-time.py
+++ solutions/merge-operations-for-minimum-travel-time.py
@@ -17,7 +17,7 @@ class Solution:
             t = 0
             for j in range(i+1, n):
                 t += time[j-1]
-                ret = min(ret, t * (position[j-1] - position[i]) + dfs(j, amt+j-i-1))
+                ret = min(ret, t * (position[j] - position[i+1]) + dfs(j, amt+j-i-1))
             dp[key] = ret
             return ret
         return dfs(0,0)
