e60fc3c9ef9fd1ff63a04b8808980608b5946567
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-04 20:47:24 -0400

    Add path-existence-queries-in-a-graph-ii (python3) - LeetCode solution

--- solutions/path-existence-queries-in-a-graph-ii.py
+++ solutions/path-existence-queries-in-a-graph-ii.py
@@ -10,10 +10,13 @@ class Solution:
             if num - pnum > maxDiff:
                 cur += 1
             comp[idx] = cur
+
         res = []
         for u, v in queries:
             if comp[u] != comp[v]:
                 res.append(-1)
             else:
+                val = abs(nums[v] - nums[u])
+                add = val//maxDiff + 1 if val < maxDiff else val//maxDiff
                 res.append(abs(nums[v] - nums[u])//maxDiff + 1)
         return res
