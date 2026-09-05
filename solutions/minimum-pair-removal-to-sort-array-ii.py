4f519ac5e620cc705610047b7f2c3e7232dcf58c
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-05 14:21:38 -0400

    Add minimum-pair-removal-to-sort-array-ii (python3) - LeetCode solution

--- solutions/minimum-pair-removal-to-sort-array-ii.py
+++ solutions/minimum-pair-removal-to-sort-array-ii.py
@@ -1,8 +1,9 @@
 class Node:
-    def __init__(self, val):
+    def __init__(self, val, idx):
         self.next = None
         self.prev = None
         self.val = val
+        self.idx = idx
 
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
@@ -11,13 +12,13 @@ class Solution:
         while i < n and nums[i] >= nums[i-1]:
             i += 1
 
-        head = Node(-inf)
-        tail = Node(inf)
+        head = Node(-inf, -1)
+        tail = Node(inf, -1)
         head.next = tail
         tail.prev = head
         curr = head 
         for i in range(len(nums)):
-            node = Node(nums[i])
+            node = Node(nums[i], i)
             curr.next = node
             node.prev = curr
             node.next = tail
@@ -49,15 +50,16 @@ class Solution:
                 rem += 1
             if nd.next.next != tail and nd.next.next.val < nd.next.val:
                 rem += 1
+
             nd.val = tot
             nxt = nd.next
             nxt.next.prev = nd
             nd.next = nxt.next
+            
             if nd.next != tail:
                 heapq.heappush(heap, (nd.val + nd.next.val, nd))
             if nd.prev != head:
                 heapq.heappush(heap, (nd.prev.val + nd.val, nd))
-
             if nd.next != tail and nd.val > nd.next.val:
                 rem -= 1
             if nd.prev != head and nd.prev.val > nd.val:
