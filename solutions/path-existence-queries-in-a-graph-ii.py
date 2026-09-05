be0e5324c97724c3e3fc79d294d3640e554082ad
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-05 11:47:11 -0400

    Add path-existence-queries-in-a-graph-ii (python3) - LeetCode solution

--- solutions/path-existence-queries-in-a-graph-ii.py
+++ solutions/path-existence-queries-in-a-graph-ii.py
@@ -10,14 +10,44 @@ class Solution:
             if num - pnum > maxDiff:
                 cur += 1
             comp[idx] = cur
+        
+        LOG = 16
+        up = [[0] * n for _ in range(LOG)]
+
+        for i in range(n):  
+            num, idx = inum[i]
+            up[0][idx] = num
+            l = i+1
+            r = n
+            while l < r:
+                m = (l+r)//2
+                num2, idx2 = inum[m]
+                if num2 - num <= maxDiff:
+                    l = m + 1
+                else:
+                    r = m
+            up[0][idx] = inum[l-1][1]
+        
+        for k in range(1, LOG):
+            for i in range(n):
+                up[k][i] = up[k-1][up[k-1][i]]
+        
+        def dist(u, v):
+            ret = 0
+            if comp[u] != comp[v]:
+                return -1
+            elif u == v:
+                return 0
+            if nums[u] > nums[v]:
+                u, v = v, u
+            for k in range(LOG-1, -1, -1):
+                nxt = up[k][u]
+                if nums[nxt] < nums[v]:
+                    u = nxt
+                    ret += 1 << k
+            return ret + 1
+
         res = []
         for u, v in queries:
-            if comp[u] != comp[v]:
-                res.append(-1)
-            elif u == v:
-                res.append(0)
-            else:
-                val = abs(nums[v] - nums[u])
-                add = ceil(val/maxDiff if maxDiff else val)
-                res.append(add if add else 1)
+            res.append(dist(u, v))
         return res
