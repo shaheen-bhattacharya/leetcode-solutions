8a6cbd3920c65ba23788002bff94b89ea733fb57
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-05 14:06:46 -0400

    Add minimum-pair-removal-to-sort-array-ii (python3) - LeetCode solution

--- solutions/minimum-pair-removal-to-sort-array-ii.py
+++ solutions/minimum-pair-removal-to-sort-array-ii.py
@@ -1,10 +1,76 @@
+class Node:
+    def __init__(self, val):
+        self.next = None
+        self.prev = None
+        self.val = val
+
 class Solution:
     def minimumPairRemoval(self, nums: List[int]) -> int:
         n = len(nums)
         i = 1
         while i < n and nums[i] >= nums[i-1]:
             i += 1
-        return n - i
+
+        head = Node(-inf)
+        tail = Node(inf)
+        head.next = tail
+        tail.prev = head
+        curr = head 
+        for i in range(len(nums)):
+            node = Node(nums[i])
+            curr.next = node
+            node.prev = curr
+            node.next = tail
+            tail.prev = node
+            curr = curr.next
+
+        ops = 0
+        heap = []
+        curr = head.next
+        bad = 0
+
+        while curr.next != tail:
+            if curr.next.val < curr.val:
+                bad += 1
+            curr = curr.next
+            heapq.heappush(heap, (curr.val + curr.next.val, curr))
+
+        if bad == 0:
+            return ops
+
+        while heap:
+            tot, nd = heapq.heappop(heap)
+            if not nd.next or tot != nd.val + nd.next.val:
+                continue
+            rem = 0
+            if nd.val > nd.next.val:
+                rem += 1
+            if nd.val < nd.prev.val:
+                rem += 1
+            if nd.next.next != tail and nd.next.next.val < nd.next.val:
+                rem += 1
+            nd.val = tot
+            nxt = nd.next
+            nxt.next.prev = nd
+            nd.next = nxt.next
+            nxt.prev = None
+            nxt.next = None
+            if nd.val > nd.next.val:
+                rem -= 1
+            if nd.prev.val > nd.val:
+                rem -= 1
+            bad -= rem
+            ops += 1
+            if bad == 0:
+                return ops
+        return ops
+
+            
+
+            
+
+            
+            
             
             
         
