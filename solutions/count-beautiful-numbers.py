f723be09f2541d1cc277930cae8bc2a66e741e87
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-07 13:37:58 -0400

    Add count-beautiful-numbers (python3) - LeetCode solution

---
+++ solutions/count-beautiful-numbers.py
@@ -0,0 +1,32 @@
+class Solution:
+    def beautifulNumbers(self, l: int, r: int) -> int:
+        """
+        123 == 321
+        """
+        def solve(num):
+            snum = str(num)
+            n = len(snum)
+            dp = {}
+            def dfs(i, tight, prod, tot):
+                key = (i, tight, prod, tot)
+                if key in dp:
+                    return dp[key]
+                if i == n:
+                    return tot == 0 or prod % tot == 0
+                digit = int(snum[i])
+                upper = digit if tight else 9
+                ret = 0
+                for j in range(upper+1):
+                    nt = False
+                    if tight and j == digit:
+                        nt = True
+                    ret += dfs(i+1, nt, prod * j, tot + j)
+                dp[key] = ret
+                return ret
+            return dfs(0, True, 1, 0)
+        return solve(r) - solve(l-1)
+                
+
+                
+
+
