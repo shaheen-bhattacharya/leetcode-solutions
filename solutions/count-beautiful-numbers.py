2964ecb6c0c3cf8ccf3784997457510bd49ac62e
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-07 13:46:19 -0400

    Add count-beautiful-numbers (python3) - LeetCode solution

--- solutions/count-beautiful-numbers.py
+++ solutions/count-beautiful-numbers.py
@@ -17,7 +17,7 @@ class Solution:
                 upper = digit if tight else 9
                 lower = 1 if i == 0 else 0
                 ret = 0
-                for j in range(0, upper+1):
+                for j in range(lower, upper+1):
                     nt = False
                     if tight and j == digit:
                         nt = True
