c94b31414e6a2959d08b7d3e8a0ff3495d9ac412
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-07 12:09:24 -0400

    Add minimum-operations-to-make-array-elements-zero (python3) - LeetCode solution

--- solutions/minimum-operations-to-make-array-elements-zero.py
+++ solutions/minimum-operations-to-make-array-elements-zero.py
@@ -14,6 +14,7 @@ class Solution:
                     tmp = r - curr + 1
                 else:
                     tmp = nxt - curr
+                print(tmp)
                 tot += tmp ** (pl+1)
                 pl += 1
                 curr = nxt
