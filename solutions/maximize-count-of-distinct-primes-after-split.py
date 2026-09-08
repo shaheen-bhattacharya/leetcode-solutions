6c4fd2b9f48e30fe13700066522d17a5ec517817
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-08 14:44:32 -0400

    Add maximize-count-of-distinct-primes-after-split (python3) - LeetCode solution

--- solutions/maximize-count-of-distinct-primes-after-split.py
+++ solutions/maximize-count-of-distinct-primes-after-split.py
@@ -1,26 +1,61 @@
 class Solution:
     def maximumCount(self, nums: List[int], queries: List[List[int]]) -> List[int]:
-        def sieve(n):
-            lp = [0] * (n+1)
-            primes = []
-            for i in range(2, n+1):
-                if lp[i] == 0:
-                    lp[i] = i
-                    primes.append(i)
-                for p in primes:
-                    if p > lp[i] or p * i > n:
-                        break
-                    lp[p*i] = p
-            return lp, primes
-
-        maxv = max(nums)
         n = len(nums)
-        lp, primes = sieve(maxv)
-        print(lp, "\n", primes)
+        tree = [0] * (4 * n)
+        lazy = [0] * (4 * n)
+        maxv = max(nums)
+        pos = defaultdict(SortedList)
 
-        # @cache
-        # def isPrime(num):
+        def push(node):
+            if lazy[node] != 0:
+                tree[2*node] += lazy[node]
+                tree[2*node+1] += lazy[node]
+                lazy[2*node] += lazy[node]
+                lazy[2*node+1] += lazy[node]
+                lazy[node] = 0
 
+        def update(ql, qr, val):
+            def dfs(node, l, r):
+                if r < ql or l > qr:
+                    return 
+                if ql <= l and r <= qr:
+                    lazy[node] += 1
+                    tree[node] += 1
+                    return
+                m = (l + r) //2
+                push(node)
+                dfs(2*node, l, m)
+                dfs(2*node+1, m+1, r)
+                tree[node] = max(tree[2*node], tree[2*node+1])
+        
+        def query(ql, qr):
+            def dfs(node, l, r):
+                if r < ql or l > qr:
+                    return 0
+                if ql <= l and r <= qr:
+                    return tree[node]
+                m = (l+r)//2
+                push(node)
+                return max(dfs(2*node, l, m), dfs(2*node+1, m+1, r))
 
-        # tree = [0] * (4 * n) 
-        # def build(node, l, r):
+        for i, num in enumerate(nums):
+            pos[num].add(i)
+
+        res = []
+        for i, val in queries:
+            old = nums[i]
+            pl, pr = pos[old][0], pos[old][-1]
+            pos[old].remove(i)
+            nums[i] = val
+            pos[val].add(i)
+            nl, nr = pos[val][0], pos[val][-1]
+            if nl < pl:
+                update(nl+1, min(pl, nr), val)
+            elif nr > pr:
+                update(max(nl, pr)+1, nr, val)
+            res.append(query(1, n))
+        return res
+
+            
+
+        
