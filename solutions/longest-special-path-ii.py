a8c5ac528af5c49e6f93ff77ff5e16a8651857b6
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-09 13:30:52 -0400

    Add longest-special-path-ii (python3) - LeetCode solution

--- solutions/longest-special-path-ii.py
+++ solutions/longest-special-path-ii.py
@@ -1,6 +1,3 @@
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def longestSpecialPath(self, edges: List[List[int]], nums: List[int]) -> List[int]:
         n = len(nums)
@@ -8,65 +5,66 @@ class Solution:
         for u, v, l in edges:
             adj[u].append((v, l))
             adj[v].append((u, l))
-            
-        best_len = 0
-        min_nodes = 1
-        
-        # Maps value -> list of depth indices where it appears along current path
-        occ = defaultdict(list)
-        
-        # Prefix distances from root to current depth
-        pref = [0]
-        
-        def dfs(node, par, hard_left, depth, dup_bounds):
-            nonlocal best_len, min_nodes
-            
-            val = nums[node]
-            occ[val].append(depth)
-            k = len(occ[val])
-            
-            # Rule 1: NO value can appear 3 or more times.
-            # If val appears 3+ times, the path CANNOT include the 1st occurrence (depth occ[val][-3]).
-            # Thus, hard_left must be pushed past occ[val][-3].
-            if k >= 3:
-                hard_left = max(hard_left, occ[val][-3] + 1)
-                
-            # Rule 2: Collect duplicate boundary constraints.
-            # A duplicate pair occurs when k >= 2 at indices (occ[val][-2], depth).
-            # To eliminate this duplicate pair, L would need to be >= occ[val][-2] + 1.
-            new_dup_bounds = list(dup_bounds)
-            if k >= 2:
-                bound = occ[val][-2] + 1
-                new_dup_bounds.append(bound)
-                new_dup_bounds.sort()
-                
-            # Rule 3: Allow AT MOST ONE duplicate pair on the path.
-            # If we have 2 or more duplicate pairs active, we MUST eliminate all but 1.
-            # Thus, effective_left must be at least the 2nd largest duplicate boundary.
-            effective_left = hard_left
-            if len(new_dup_bounds) >= 2:
-                effective_left = max(effective_left, new_dup_bounds[-2])
-
-            # Calculate current path length and node count
-            path_len = pref[depth] - pref[effective_left]
-            node_cnt = depth - effective_left + 1
-            
-            if path_len > best_len:
-                best_len = path_len
-                min_nodes = node_cnt
-            elif path_len == best_len:
-                min_nodes = min(min_nodes, node_cnt)
-                
-            # Recursively traverse children
-            for nei, w in adj[node]:
+        pref = [0] * n
+        def pc(node, par):
+            for nei, l in adj[node]:
                 if nei == par:
                     continue
-                pref.append(pref[-1] + w)
-                dfs(nei, node, hard_left, depth + 1, new_dup_bounds)
-                pref.pop()
-                
-            # Backtrack state
-            occ[val].pop()
+                pref[nei] = pref[node] + l
+                pc(nei, node)
 
-        dfs(0, -1, 0, 0, [])
-        return [best_len, min_nodes]
+        pc(0, -1)
+
+        best = 0
+        mn = inf
+        def dfs(node, par, occ, left, path):
+            nonlocal best
+            nonlocal mn
+            nl = left
+            val = nums[node]
+            path.append(node)
+            occ[val].append(len(path) - 1)
+            if len(occ[val]) >= 2 and left < occ[val][-2]:
+                v = occ[val][-2]
+                if v < sml:
+                    sml2 = sml
+                    sml = v
+                elif v < sml2:
+                    sml2 = v
+            nl = max(left, sml + 1) if sml != inf else left
+            tot = pref[node] - pref[path[nl]]
+            if tot > best:
+                best = tot
+                mn = min(mn, len(path) - nl)
+            
+            for nei, l in adj[node]:
+                if nei == par:
+                    continue
+                dfs(nei, node, occ, nl, path)
+            path.pop()
+            occ[val].pop()
+        
+        dfs(0, -1, defaultdict(list), 0, [])
+        return [best, mn]
+
+
+            
+            
+
+            
+            
+
+            
+
+
+
+
+            
+
+
+
+
+
+                    
+
+                
