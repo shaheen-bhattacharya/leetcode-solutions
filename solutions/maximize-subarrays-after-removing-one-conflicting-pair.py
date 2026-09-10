0ed52085866d6f48f13e389862e2d58e3eae470a
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-10 13:47:38 -0400

    Add maximize-subarrays-after-removing-one-conflicting-pair (python3) - LeetCode solution

--- solutions/maximize-subarrays-after-removing-one-conflicting-pair.py
+++ solutions/maximize-subarrays-after-removing-one-conflicting-pair.py
@@ -2,16 +2,34 @@ class Solution:
     def maxSubarrays(self, n: int, conflictingPairs: List[List[int]]) -> int:
         #1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
         #[4, 8]
-        tot = (n * (n+1)) // 2
-        res = 0
-        corr = {}
+
+        fst = [0] * (n+1) #fst[i] = earlierst l pos for l to i be valid 
+        snd = [0] * (n+1)
+
         for a, b in conflictingPairs:
-            ntot = tot
-            v1 = a-1
-            v2 = n-b
-            if v1 == 0 and v2 == 0:
-                ntot = 0
-            else:
-                ntot -= max(v1, 1) * max(v2, 1)
-            corr[(a, b)] = ntot
-        print(corr)
+            if a > b:
+                a, b = b, a
+            if a + 1 > fst[b]:
+                snd[b] = fst[b]
+                fst[b] = a+1
+            elif a+1 > snd[b]:
+                snd[b] = a+1
+        tot = 0
+        for i in range(1, n+1):
+            l = fst[i] - i + 1
+            tot += (l * (l+1))//2
+
+        res = 0
+        for a, b in conflictingPairs:
+            if a > b:
+                a, b = b, a
+            pl = fst[b] - b + 1
+            ntot = tot - (pl * (pl+1))//2
+            nl = fst[b] - b + 1
+            if fst[b] == a + 1:
+                nl = snd[b] - b + 1
+            ntot += (nl * (nl+1))//2
+            res = max(res, ntot)
+        return res
+                
+
