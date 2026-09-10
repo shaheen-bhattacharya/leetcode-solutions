c17bfb1e71613df2c29b782af39437d5dbedb050
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-10 16:35:50 -0400

    Add maximize-subarrays-after-removing-one-conflicting-pair (python3) - LeetCode solution

--- solutions/maximize-subarrays-after-removing-one-conflicting-pair.py
+++ solutions/maximize-subarrays-after-removing-one-conflicting-pair.py
@@ -3,26 +3,30 @@ class Solution:
         #1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
         #[4, 8]
 
+        f = [1]*(n+1)
+        fst = [1] * (n+1)
+        snd = [1] * (n+1)
+        
         pairs = defaultdict(list)
         for i, (a, b) in enumerate(conflictingPairs):
             if a > b:
                 a, b = b, a
-            pairs[b].append((a+1, i))
-        
-        gain = [0] * len(conflictingPairs)
-        print(pairs)
+            pairs[b].append(a)
         
         mx1 = 1
         mx2 = 1
-        tot = 0
-        # for i in range(1, n+1):
-        #     for l, idx in pairs[i]:
-        #         if l > mx1:
-        #             mx2 = mx1
-        #             mx1 = l
-        #         elif l > mx2:
-        #             mx2 = l
+        for i in range(1, n+1):
+            for l in pairs[i]:
+                if l > mx1:
+                    mx2 = mx1
+                    mx1 = l
+                elif l > mx2:
+                    mx2 = l
+            fst[i] = mx1
+            snd[i] = mx2
+        print(fst)
+
+
                 
-
-
-            
+        
+        
