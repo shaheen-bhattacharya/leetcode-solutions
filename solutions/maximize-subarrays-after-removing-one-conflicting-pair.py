c363390e006686e207551ef9a9d08484d39d419f
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-10 16:03:52 -0400

    Add maximize-subarrays-after-removing-one-conflicting-pair (python3) - LeetCode solution

--- solutions/maximize-subarrays-after-removing-one-conflicting-pair.py
+++ solutions/maximize-subarrays-after-removing-one-conflicting-pair.py
@@ -3,35 +3,26 @@ class Solution:
         #1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
         #[4, 8]
 
-        fst = [1] * (n+1) #fst[i] = earlierst l pos for l to i be valid 
-        snd = [1] * (n+1)
-
-        for a, b in conflictingPairs:
+        pairs = defaultdict(list)
+        for i, (a, b) in enumerate(conflictingPairs):
             if a > b:
                 a, b = b, a
-            if a + 1 > fst[b]:
-                snd[b] = fst[b]
-                fst[b] = a+1
-            elif a+1 > snd[b]:
-                snd[b] = a+1
-        print(fst)
-        print(snd)
+            pairs[b].append((a+1, i))
+        
+        gain = [0] * len(conflictingPairs)
+        print(pairs)
+        
+        mx1 = 1
+        mx2 = 1
         tot = 0
-        for i in range(1, n+1):
-            l = fst[i] - i + 1
-            tot += (l * (l+1))//2
-
-        res = 0
-        for a, b in conflictingPairs:
-            if a > b:
-                a, b = b, a
-            pl = fst[b] - b + 1
-            ntot = tot - (pl * (pl+1))//2
-            nl = fst[b] - b + 1
-            if fst[b] == a + 1:
-                nl = snd[b] - b + 1
-            ntot += (nl * (nl+1))//2
-            res = max(res, ntot)
-        return res
+        # for i in range(1, n+1):
+        #     for l, idx in pairs[i]:
+        #         if l > mx1:
+        #             mx2 = mx1
+        #             mx1 = l
+        #         elif l > mx2:
+        #             mx2 = l
                 
 
+
+            
