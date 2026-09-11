863b6ff7df76ab7fe0a5271d2d6c0b8b61536b91
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-10 23:44:12 -0400

    Add find-maximum-non-decreasing-array-length (python3) - LeetCode solution

--- solutions/find-maximum-non-decreasing-array-length.py
+++ solutions/find-maximum-non-decreasing-array-length.py
@@ -10,9 +10,9 @@ class Solution:
             while i < n and curr < prev:
                 curr += nums[i]
                 i += 1
+            print(curr)
             tot += 1
             prev = curr
             if curr < prev:
-                print("d")
                 tot -= 1
         return tot
