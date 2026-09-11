75a53f78e79a0b56ff377f67b26c2f029b65a365
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-11 10:39:15 -0400

    Add coin-change (python3) - LeetCode solution

---
+++ solutions/coin-change.py
@@ -0,0 +1,9 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [inf] * (amount + 1)
+        dp[0]
+        for c in coins:
+            for i in range(1, amount+1):
+                if i - c >= 0:
+                    dp[amount] = min(dp[amount], dp[i-c]+1)
+        return dp[amount] if dp[amount] != inf else -1
