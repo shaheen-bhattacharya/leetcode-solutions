d405c02d4647c756e3c7e2f2148135a4b345afb5
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-11 23:34:24 -0400

    Add brace-expansion-ii (python3) - LeetCode solution

--- solutions/brace-expansion-ii.py
+++ solutions/brace-expansion-ii.py
@@ -39,6 +39,8 @@ class Solution:
                     need.append(dfs(s+1, i-2))
                     end = False
                 else:
+                    if (l, r) == (1, 3):
+                        print(need)
                     res |= calc(need)
                     need = []
                     i += 1
