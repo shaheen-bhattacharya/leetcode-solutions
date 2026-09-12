c73d791bf550d73bf121d36ea2b23dbb6b3ae596
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-12 15:41:50 -0400

    Add minimum-days-to-score-exactly-n-points (python3) - LeetCode solution

---
+++ solutions/minimum-days-to-score-exactly-n-points.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minDays(self, n: int) -> int:
+        #dp[i] = min # of days to score i
+        upper = 0
+        cp = 1
+        tmp = n
+        while tmp > 0:
+            tmp -= cp
+            upper += 1
+            cp += 1
+        
+        tri = [0] * (upper+1)
+        for i in range(1, upper+1):
+            tri[i] = tri[i-1] + i
+
+        dp = [inf] * (n+1)
+        dp[0] = 0
+            
+        for i in range(1, n):
+            for l in range(1, upper+1):
+                if i + tri[l] > n:
+                    break
+                dp[i+tri[l]] = min(dp[i+tri[l]], l + dp[i]+1)
+                
+        # print(dp)
+        return dp[n]
+                
+        
+        
+        
+                
+        
+        
+        
