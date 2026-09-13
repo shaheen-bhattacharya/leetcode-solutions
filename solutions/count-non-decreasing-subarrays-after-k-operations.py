99ba28986c40093d4edc2ea98777bd2732151b86
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-13 19:07:44 -0400

    Add count-non-decreasing-subarrays-after-k-operations (python3) - LeetCode solution

--- solutions/count-non-decreasing-subarrays-after-k-operations.py
+++ solutions/count-non-decreasing-subarrays-after-k-operations.py
@@ -39,7 +39,7 @@ class Solution:
                     l = m + 1
                 else:
                     r = m
-            rops = r - i
+            rops = r - i - 1
             if rops == 1:
                 rops += dpr[r-1]
             res += lops * rops
