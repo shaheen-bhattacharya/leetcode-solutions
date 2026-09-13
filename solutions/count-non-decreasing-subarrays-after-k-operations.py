8c5bdac966c7d7ea7c0d43d424c8ef8af88853cd
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-13 18:13:42 -0400

    Add count-non-decreasing-subarrays-after-k-operations (python3) - LeetCode solution

---
+++ solutions/count-non-decreasing-subarrays-after-k-operations.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countNonDecreasingSubarrays(self, nums: List[int], k: int) -> int:
+        stack = []
+        n = len(nums)
+        pref = [0] + list(accumulate(nums))
+        
+        left = [-1] * n
+        for i in range(n):
+            while stack and nums[i] > nums[stack[-1]]:
+                stack.pop()
+            if stack:
+                left[i] = stack[-1]
+            stack.append(i)
+
+        right = [n] * n
+        for i in range(n-1, -1, -1):
+            while stack and nums[i] >= nums[stack[-1]]:
+                stack.pop()
+            if stack:
+                right[i] = stack[-1]
+            stack.append(i)
+        
+        res = 0
+        for i in range(n):
+            lops = i - left[i] - 1
+            l, r = i+1, right[i]
+            while l < r:
+                m = (l+r)//2
+                if pref[m+1] - pref[i+1] <= 4 * (m-i) + k:
+                    l = m + 1
+                else:
+                    r = m
+            rops = min(right[i], r) - i - 1
+            res += lops * rops
+        return res
+
