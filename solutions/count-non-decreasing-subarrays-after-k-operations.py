9efd5939e3dcd139d72c21ecfc995c6dff623a7d
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-13 18:36:28 -0400

    Add count-non-decreasing-subarrays-after-k-operations (python3) - LeetCode solution

--- solutions/count-non-decreasing-subarrays-after-k-operations.py
+++ solutions/count-non-decreasing-subarrays-after-k-operations.py
@@ -10,7 +10,7 @@ class Solution:
 
         dpr = [1] * n
         for i in range(n-2, -1, -1):
-            if nums[i] < nums[i+1]:
+            if nums[i] <= nums[i+1]:
                 dpr[i] = dpr[i+1] + 1
         
         left = [-1] * n
