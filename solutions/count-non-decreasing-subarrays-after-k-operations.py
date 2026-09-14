96ff1543595ea811b72a0713226de0c751e3d8d9
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-13 20:15:25 -0400

    Add count-non-decreasing-subarrays-after-k-operations (python3) - LeetCode solution

--- solutions/count-non-decreasing-subarrays-after-k-operations.py
+++ solutions/count-non-decreasing-subarrays-after-k-operations.py
@@ -1,48 +1,34 @@
 class Solution:
     def countNonDecreasingSubarrays(self, nums: List[int], k: int) -> int:
-        stack = []
         n = len(nums)
-        pref = [0] + list(accumulate(nums))
-        dpl = [1] * n
-        for i in range(1, n):
-            if nums[i] > nums[i-1]:
-                dpl[i] = dpl[i-1] + 1
-        dpr = [1] * n
-        for i in range(n-2, -1, -1):
-            if nums[i] <= nums[i+1]:
-                dpr[i] = dpr[i+1] + 1
-        
-        left = [-1] * n
-        for i in range(n):
-            while stack and nums[i] > nums[stack[-1]]:
-                stack.pop()
-            if stack:
-                left[i] = stack[-1]
-            stack.append(i)
+        q = deque([(nums[-1], 1)])
+        cost = 0
+        r = len(nums) - 1
 
-        stack = []
-        right = [n] * n
-        for i in range(n-1, -1, -1):
-            while stack and nums[i] >= nums[stack[-1]]:
-                stack.pop()
-            if stack:
-                right[i] = stack[-1]
-            stack.append(i)
-        
         res = 0
-        for i in range(n):
-            lops = min(i - left[i], dpl[i])
-            l, r = i+1, right[i]
-            while l < r:
-                m = (l+r)//2
-                if nums[i] * (m-i) - (pref[m+1] - pref[i+1]) <= k:
-                    l = m + 1
-                else:
-                    r = m
-            rops = r - i - 1
-            if rops == 0:
-                rops += dpr[r-1]
-            res += lops * rops
-            # print(lops, rops, res)
+        for i in range(n-2, -1, -1):
+            c=1
+            #process left
+            while q and q[-1][0] < nums[i]:
+                prev, pc = q.pop()
+                c += pc
+                cost += pc * (nums[i] - prev)
+            q.append((nums[i], c))
+
+            #process right
+            while cost > k:
+                top, tc = q.popleft()
+                r -= 1
+                tc -= 1
+                cost -= (top - nums[r])
+                if tc > 0:
+                    q.append((top, tc))
+            res += r - i + 1
         return res
+                
+
+
+
+
+
 
