307017682623869eccf7d137e6a5a77c7aa9fd7f
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-15 14:53:37 -0400

    Add valid-arrangement-of-pairs (python3) - LeetCode solution

--- solutions/valid-arrangement-of-pairs.py
+++ solutions/valid-arrangement-of-pairs.py
@@ -9,9 +9,9 @@ class Solution:
             scorr[s].append(i)
             ecorr[e].append(i)
         print(scorr, ecorr)
-        
+
+        print(n)
         for i in range(n):
-            print(i)
             print(pairs[i], ecorr[pairs[i][1]])
             if len(ecorr[pairs[i][1]]) % 2 == 1:
                 start = i
