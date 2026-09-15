a505b4f1b3a6ca107e00669f90365d39f2fd2844
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-15 14:26:03 -0400

    Add reconstruct-itinerary (python3) - LeetCode solution

--- solutions/reconstruct-itinerary.py
+++ solutions/reconstruct-itinerary.py
@@ -2,12 +2,12 @@ class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         adj = defaultdict(list)
         for u, v in tickets:
-            adj[u].append(v)
+            heapq.heappush(adj[u], v)
 
         res = []
         def dfs(node):
             while adj[node]:
-                nei = adj[node].pop()
+                nei = heapq.heappop(adj[node])
                 dfs(nei)
             res.append(node)
         dfs("JFK")
