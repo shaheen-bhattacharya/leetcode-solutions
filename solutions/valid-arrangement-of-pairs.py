ce7d66af3c340d53e58e1e9cbf077eab9ca93cf8
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-15 14:46:09 -0400

    Add valid-arrangement-of-pairs (python3) - LeetCode solution

---
+++ solutions/valid-arrangement-of-pairs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        n = len(pairs)
+        adj = defaultdict(list)
+        scorr = defaultdict(list)
+        ecorr = defaultdict(list)
+        for i, (s, e) in enumerate(pairs):
+            adj[i] = ecorr[e]
+        start = 0
+        for i in range(n):
+            if len(adj[i]) % 2 == 1:
+                start = i
+                break
+        res = []
+        def dfs(node):
+            while adj[node]:
+                nei = adj[node].pop()
+                dfs(nei)
+            res.append(pairs[i])
+        dfs(start)
+        return res
