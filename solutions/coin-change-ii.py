7aef9c2c586447ef7eea96558694f20f0e8bb6a3
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-16 17:13:49 -0400

    Add coin-change-ii (python3) - LeetCode solution

--- solutions/coin-change-ii.py
+++ solutions/coin-change-ii.py
@@ -3,7 +3,7 @@ class Solution:
         dp = [0] * (amount + 1)
         dp[0] = 1
 
-        for c in range(amount, -1, -1):
+        for c in range(amount+1):
             for v in coins:
                 if c - v >= 0:
                     dp[c] += dp[c-v]
