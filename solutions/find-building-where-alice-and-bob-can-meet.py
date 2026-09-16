ee2d231a110d5c00c61152635138bced0b796009
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-16 13:39:27 -0400

    Add find-building-where-alice-and-bob-can-meet (python3) - LeetCode solution

---
+++ solutions/find-building-where-alice-and-bob-can-meet.py
@@ -0,0 +1,44 @@
+class Solution:
+    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
+        stack = []
+        nq = len(queries)
+        n = len(heights)
+        for i in range(nq):
+            a, b = queries[i]
+            if a > b:
+                a, b = b, a
+            queries[i] = [a, b, i]
+        queries.sort(key = lambda x: x[1])  
+
+        res = [0] * (nq)
+
+        hi = n-1
+        for i in range(nq-1, -1, -1):
+            if a == b:
+                continue
+            a, b, ix = queries[i]
+            for j in range(hi, b-1, -1):
+                while stack and heights[j] > heights[stack[-1][0]]:
+                    stack.pop()
+                stack.append((heights[j], j))
+
+            l, r = 0, len(stack) 
+            need = max(heights[a], heights[b])
+            while l < r:
+                m = (l + r) // 2
+                hgt, idx = stack[m]
+                if hgt < need:
+                    l = m + 1
+                else:
+                    r = m
+            if l == len(stack):
+                res[ix] = -1
+            else:
+                res[ix] = stack[l][1]
+        return res
+            
+                
+
+
+
+
