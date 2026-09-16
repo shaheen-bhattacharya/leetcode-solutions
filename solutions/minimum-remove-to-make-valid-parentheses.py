cf91a82682800aa4985bc8c8c4de564ad44e2110
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-16 16:48:07 -0400

    Add minimum-remove-to-make-valid-parentheses (python3) - LeetCode solution

---
+++ solutions/minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        sarr = list(s)
+        stack = []
+        diff = 0
+        for i, ch in enumerate(s):
+            if ch == "(":
+                diff += 1
+                stack.append((ch, i))
+            elif ch == ")":
+                if diff == 0:
+                    continue
+                stack.append((ch, i))
+                diff -= 1
+
+        while stack and diff > 0:
+            ch, i = stack.pop()
+            if ch == "(":
+                continue
+            else:
+                sarr[i] = ""
+                diff -= 1
+        return "".join(sarr)
+            
+
+            
