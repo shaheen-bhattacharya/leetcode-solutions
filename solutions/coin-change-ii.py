af588c81417fd9218157770a1fd56042befc2b1d
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-16 17:25:51 -0400

    Add coin-change-ii (python3) - LeetCode solution

--- solutions/coin-change-ii.py
+++ solutions/coin-change-ii.py
@@ -3,11 +3,10 @@ class Solution:
         dp = [0] * (amount + 1)
         dp[0] = 1
         sc = set(coins)
-        coins.sort()
 
         for c in range(amount+1):
             for v in coins:
-                if c > v*2:
+                if c < v*2:
                     continue
                 if c - v >= 0:
                     dp[c] += dp[c-v]
