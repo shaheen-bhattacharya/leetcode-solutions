c69e4c02fafed8532391f58893c2b175d05b329b
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-16 18:23:20 -0400

    Add coin-change-ii (python3) - LeetCode solution

--- solutions/coin-change-ii.py
+++ solutions/coin-change-ii.py
@@ -2,15 +2,9 @@ class Solution:
     def change(self, amount: int, coins: list[int]) -> int:
         dp = [0] * (amount + 1)
         dp[0] = 1
-        sc = set(coins)
-        coins.sort(reverse=True)
-
-        for c in range(amount+1):
-            used = set()
-            for v in coins:
-                if c - v >= 0 and v not in used:
-                    used.add(v)
-                    used.add(c-v)
-                    dp[c] += dp[c-v]
-        print(dp)
+        
+        for c in coins:
+            for i in range(1, amount+1):
+                if i - c >= 0:
+                    dp[i] += dp[i-c]
         return dp[amount]
