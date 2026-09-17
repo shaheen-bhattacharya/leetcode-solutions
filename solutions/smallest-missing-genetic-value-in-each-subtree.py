a786e5b6694b438d38be45267f9ac0b24500fd96
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-16 21:29:59 -0400

    Add smallest-missing-genetic-value-in-each-subtree (python3) - LeetCode solution

--- solutions/smallest-missing-genetic-value-in-each-subtree.py
+++ solutions/smallest-missing-genetic-value-in-each-subtree.py
@@ -1,20 +1,34 @@
 class Solution:
     def smallestMissingValueSubtree(self, parents: List[int], nums: List[int]) -> List[int]:
-        sl = SortedList([i for i in range(1, 100001)])
         adj = defaultdict(list)
         n = len(parents)
         for i in range(n):
             adj[parents[i]].append(i)
             adj[i].append(parents[i])
+        rev = {}
+        for i in range(n):
+            rev[nums[i]] = i
 
-        res = [0] * n
-        def dfs(node):
+        def gset(node):
+            ret = set([node])
             for nei in adj[node]:
                 if nei == parents[node]:
                     continue
-                dfs(nei)
-            sl.remove(nums[node])
-            res[node] = sl[0]
-        dfs(0)
+                ret |= gset(nei)
+            return ret
+        
+        res = [0] * n
+        def dfs(mv, allowed):
+            if len(allowed) == 0:
+                return 
+            node = rev[mv] if mv in rev else -2
+            rem = gset(node) if node != -2 else set()
+            allowed -= rem
+            for nd in allowed:
+                res[nd] = mv
+            dfs(mv+1, rem)
+
+        dfs(1, set([i for i in range(n)]))
         return res
+
 
