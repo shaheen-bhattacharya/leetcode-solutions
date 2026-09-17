7a73e4324b9655322aef13aba87bbe05944228dd
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-17 09:44:46 -0400

    Add longest-increasing-subsequence (python3) - LeetCode solution

---
+++ solutions/longest-increasing-subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLIS(self, nums: list[int]) -> int:
+        tail = []
+        n = len(nums)
+        sl = SortedList()
+
+        res = 0
+        for i in range(n):
+            sl.add(nums[i])
+            idx = sl.bisect_left(nums[i])
+            res = max(res, idx)
+        return res
