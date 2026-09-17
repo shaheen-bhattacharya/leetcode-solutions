c5d130cb36dbb18dd43a462e82664f073ccca5cf
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-16 23:03:26 -0400

    Add smallest-missing-genetic-value-in-each-subtree (python3) - LeetCode solution

--- solutions/smallest-missing-genetic-value-in-each-subtree.py
+++ solutions/smallest-missing-genetic-value-in-each-subtree.py
@@ -2,36 +2,27 @@ class Solution:
     def smallestMissingValueSubtree(self, parents: List[int], nums: List[int]) -> List[int]:
         adj = defaultdict(list)
         n = len(parents)
-        indegree = [0] * n
-        for i in range(1, n):
-            adj[parents[i]].append(i)
-            adj[i].append(parents[i])
-            indegree[parents[i]] += 1
-        # print(indegree)
-        rev = {}
+        res = [1] * n
+        seen = [0] * 100001
+        if 1 not in nums:
+            return res
         for i in range(n):
-            rev[nums[i]] = i
+            adj[parents[i]].append(i)
+        
+        def dfs(i):
+            for ch in adj[i]:
+                dfs(ch)
+            seen[nums[i]] = 1
+    
+        i = nums.index(1)
+        miss = 1
 
-        res = [0] * n
-        maxv = [1] * n
-        sets = defaultdict(set)
-        q = deque([i for i in range(n) if indegree[i] == 0])
-
-        while q:    
-            node = q.popleft()
-            if node == -1:
-                break
-            sets[node].add(nums[node])
-            sets[parents[node]] |= sets[node]
-            miss = maxv[node]
-            while miss in sets[node]:
+        while i >= 0:
+            dfs(i)
+            while seen[miss]:
                 miss += 1
-            res[node] = miss
-            maxv[parents[node]] = max(maxv[parents[node]], miss)
-            indegree[parents[node]] -= 1
-            if indegree[parents[node]] == 0:
-                q.append(parents[node])
+            res[i] = miss
+            i = parents[i]
         return res
-
             
 
