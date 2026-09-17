1fe9768e7e7e29c3be0f84bf547008cfee749c08
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-17 09:45:18 -0400

    Add longest-increasing-subsequence (python3) - LeetCode solution

--- solutions/longest-increasing-subsequence.py
+++ solutions/longest-increasing-subsequence.py
@@ -7,6 +7,7 @@ class Solution:
         res = 0
         for i in range(n):
             sl.add(nums[i])
+            print(nums[i], sl, i)
             idx = sl.bisect_left(nums[i])
             res = max(res, idx)
         return res
