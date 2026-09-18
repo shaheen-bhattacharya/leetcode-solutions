9ba7a53c6e4c8f04a5b6c21d0558b4385d1e8aa8
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-18 14:53:37 -0400

    Add sum-of-distances-in-tree (python3) - LeetCode solution

--- solutions/sum-of-distances-in-tree.py
+++ solutions/sum-of-distances-in-tree.py
@@ -18,13 +18,17 @@ class Solution:
             sizes[node] = nodes
             return nodes, dist[node]
         dfs(0, -1)
-        print(sizes)
+        # print(sizes)
 
-        # def dfs2(node, par, curr):
-        #     for nei in adj[node]:
-        #         if nei == par:
-        #             continue
-                
+        def dfs2(node, par):
+            for nei in adj[node]:
+                if nei == par:
+                    continue
+                dist[nei] = dist[node] + n - 2 * sizes[nei]
+                dfs2(nei, node)
+        dfs2(0, -1)
+        return dist
+
         
 
 
