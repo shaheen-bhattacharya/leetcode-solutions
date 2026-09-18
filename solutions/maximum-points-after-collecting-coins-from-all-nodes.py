41a9fe3a492b3e1a83ed614b699e4dd26fa0a006
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-18 11:32:00 -0400

    Add maximum-points-after-collecting-coins-from-all-nodes (python3) - LeetCode solution

--- solutions/maximum-points-after-collecting-coins-from-all-nodes.py
+++ solutions/maximum-points-after-collecting-coins-from-all-nodes.py
@@ -2,28 +2,36 @@ class Solution:
     def maximumPoints(self, edges: List[List[int]], coins: List[int], k: int) -> int:
         n = len(edges) + 1
         adj = defaultdict(list)
+        pars = {}
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
+        def dfs2(node, par):
+            pars[node] = par
+            for nei in adj[node]:
+                if nei == par:
+                    continue
+                dfs2(nei, node)
+        dfs2(0, -1)
         dp = {}
 
-        def dfs(node, par, mods):
-            key = (node, par, mods) 
+        def dfs(node, mods):
+            key = (node, mods) 
             if key in dp:
                 return dp[key]
 
             op1 = coins[node]//2**mods - k
             for nei in adj[node]:
-                if nei == par:
+                if nei == pars[node]:
                     continue
-                op1 += dfs(nei, node, mods)
+                op1 += dfs(nei, mods)
 
             op2 = coins[node]//2**(mods+1)
             for nei in adj[node]:
-                if nei == par:
+                if nei == pars[node]:
                     continue
-                op2 += dfs(nei, node, mods+1)
+                op2 += dfs(nei, mods+1)
 
             dp[key] = max(op1, op2)
             return dp[key]
-        return dfs(0, -1, 0)            
+        return dfs(0, 0)            
