0970077a846c251473a17dba80f7dabbf6e4019e
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-19 22:50:30 -0400

    Add longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit (python3) - LeetCode solution

--- solutions/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ solutions/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -36,6 +36,7 @@ class Solution:
             if ustack:
                 rpos = ustack[-1]
             ustack.append(i)
+            print(i, rpos, r)
             res = max(res, min(rpos, r) - max(lpos[i], left[i]) - 1)
         return res
 
