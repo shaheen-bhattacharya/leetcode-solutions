f9227ff7533cc963c9bceee3e0aa7caf78906269
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-19 21:11:09 -0400

    Add increment-submatrices-by-one (python3) - LeetCode solution

---
+++ solutions/increment-submatrices-by-one.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rangeAddQueries(self, n: int, queries: list[list[int]]) -> list[list[int]]:
+        lazy = [[0] * n for _ in range(n)]
+
+        for r1, c1, r2, c2 in queries:
+            lazy[r1][c1] += 1
+            lazy[r2][c2] -= 1
+
+        res = [[0]*n for _ in range(n)]
+        
+        cur = 0
+        for r in range(n):
+            for c in range(n):
+                cur += lazy[r][c]
+                res[r][c] = cur
+        return cur
+                
+
+
