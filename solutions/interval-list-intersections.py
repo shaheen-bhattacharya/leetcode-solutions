dbf035f5587762de90b1fc6c26b9113f2c6d0944
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-19 20:05:09 -0400

    Add interval-list-intersections (python3) - LeetCode solution

---
+++ solutions/interval-list-intersections.py
@@ -0,0 +1,19 @@
+class Solution:
+    def intervalIntersection(self, firstList: list[list[int]], secondList: list[list[int]]) -> list[list[int]]:
+        fp = 0
+        sp = 0
+        res = []
+        while fp < len(firstList) and sp < len(secondList):
+            fs, fe = firstList[fp]
+            ss, se = secondList[sp]
+            s, e = max(fs, ss), min(fe, se)
+            if s <= e:
+                res.append([s, e])
+            if fe <= se:
+                sp += 1
+            else:
+                fe += 1
+        return res                    
+
+
+
