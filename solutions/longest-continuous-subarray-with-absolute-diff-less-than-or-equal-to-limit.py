a268baf3ccd3d9096ee1549c126b9ed578d7b19d
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-19 22:45:12 -0400

    Add longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit (python3) - LeetCode solution

---
+++ solutions/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,42 @@
+class Solution:
+    def longestSubarray(self, nums: list[int], limit: int) -> int:
+        stack = []
+        ustack = []
+        n = len(nums)
+        left = [-1] * n
+        lpos = [-1] * n
+        for i, num in enumerate(nums):
+            val = num - limit
+            while stack and val <= nums[stack[-1]]:
+                stack.pop()
+            if stack:
+                left[i] = stack[-1]
+            stack.append(i)
+            while ustack and num >= nums[ustack[-1]]:
+                ustack.pop()
+            if ustack:
+                lpos = ustack[-1]
+        
+        res = 0
+        stack = []
+        ustack = []
+        for i in range(n-1, -1, -1):
+            val = num - limit
+            rpos, r = -1, -1
+            while stack and val < nums[stack[-1]]:
+                stack.pop()
+            if stack:
+                r = stack[-1]
+            stack.append(i)
+            while ustack and num > nums[ustack[-1]]:
+                ustack.pop()
+            if ustack:
+                rpos = ustack[-1]
+            res = max(res, min(rpos, r) - max(lpos[i], left[i]) - 1)
+        return res
+
+        
+            
+
+            
+            
