4ac9f559f40b0c357e01f22b1b41f8f72ac0d21c
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-19 21:48:31 -0400

    Add increment-submatrices-by-one (python3) - LeetCode solution

--- solutions/increment-submatrices-by-one.py
+++ solutions/increment-submatrices-by-one.py
@@ -6,6 +6,7 @@ class Solution:
             diff[r1][c1] += 1
             diff[r2+1][c2] -= 1
             diff[r2][c2+1] -= 1
+            diff[r2+1][c2+1] += 1
 
         res = [[0]*n for _ in range(n)]
         
@@ -16,7 +17,7 @@ class Solution:
                 if c > 0:
                     diff[r][c] += diff[r][c-1]
                 if r > 0 and c > 0:
-                    diff[r][c] += diff[r-1][c-1]
+                    diff[r][c] -= diff[r-1][c-1]
                 res[r][c] = diff[r][c]
         return res
                 
