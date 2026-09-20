1189d34731a131cb5bf8ec53cde94d1f555f0f89
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-19 21:22:25 -0400

    Add increment-submatrices-by-one (python3) - LeetCode solution

--- solutions/increment-submatrices-by-one.py
+++ solutions/increment-submatrices-by-one.py
@@ -1,22 +1,26 @@
 class Solution:
     def rangeAddQueries(self, n: int, queries: list[list[int]]) -> list[list[int]]:
-        ladd = [[0] * n for _ in range(n)]
-        lsub = [[0] * n for _ in range(n)]
-
+        radd = [0] * n
+        rsub = [0] * n
+        cadd = [0] * n
+        csub = [0] * n
         for r1, c1, r2, c2 in queries:
-            ladd[r1][c1] += 1
-            lsub[r2][c2] += 1
+            radd[r1] += 1
+            rsub[r2] += 1
+            cadd[r1] += 1
+            csub[r2] += 1
 
         res = [[0]*n for _ in range(n)]
-        print(ladd)
-        print(lsub)
+
         
         cur = 0
         for r in range(n):
+            cur += radd[r]
             for c in range(n):
-                cur += ladd[r][c]
+                cur += cadd[c]
                 res[r][c] = cur
-                cur -= lsub[r][c]
+                cur -= csub[c]
+            cur -= rsub[r]
         return res
                 
 
