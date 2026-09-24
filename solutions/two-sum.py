acb5a1eb535aad97c66a1252b894e3276125f538
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-24 16:14:29 -0400

    Add two-sum (python) - LeetCode solution

--- solutions/two-sum.py
+++ solutions/two-sum.py
@@ -2,8 +2,8 @@ class Solution(object):
     def twoSum(self, nums, target):
         seen = {}
         for i, num in enumerate(nums):
-            if num in seen:
-                return [i, seen[num]]
+            if target-num in seen:
+                return [i, seen[target-num]]
             seen[num] = i
 
         
