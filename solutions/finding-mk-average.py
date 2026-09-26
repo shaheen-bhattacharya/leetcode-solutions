c1bcd09933059fcd4b679fda116e4606fdb2ff2f
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-26 13:13:35 -0400

    Add finding-mk-average (python3) - LeetCode solution

--- solutions/finding-mk-average.py
+++ solutions/finding-mk-average.py
@@ -40,7 +40,7 @@ class MKAverage:
 
     def calculateMKAverage(self) -> int:
         # print(self.tot, self.bad, self.sl)
-        return (self.tot - self.bad) // self.m if len(self.sl) >= self.m else -1
+        return (self.tot - self.bad) // (self.m - 2*self.k) if len(self.sl) >= self.m else -1
 
 
 # Your MKAverage object will be instantiated and called as such:
