3404c5c58a1b2fb7ac3d3ef4eef4c673e3a97e7d
shaheen-bhattacharya <shaheen.bhattacharya1773@gmail.com>
2026-09-26 13:10:37 -0400

    Add finding-mk-average (python3) - LeetCode solution

--- solutions/finding-mk-average.py
+++ solutions/finding-mk-average.py
@@ -2,6 +2,7 @@ class MKAverage:
 
     def __init__(self, m: int, k: int):
         self.sl = SortedList()
+        self.q = deque()
         self.tot = 0
         self.bad = 0
         self.m = m
@@ -10,19 +11,31 @@ class MKAverage:
     def addElement(self, num: int) -> None:
         k = self.k
         self.tot += num
+        self.q.append(num)
         if len(self.sl) < 2*k:
             self.sl.add(num)
             self.bad += num
             return
-
-        idx = self.sl.bisect_left(num)
-        if idx < k:
-            self.bad += num
-            self.bad -= self.sl[k-1]
-        elif idx > len(self.sl) - k:
-            self.bad += num
-            self.bad -= self.sl[len(self.sl) - k]
-        self.sl.add(num)
+        if len(self.q) < self.m:
+            idx = self.sl.bisect_left(num)
+            if idx < k:
+                self.bad += num
+                self.bad -= self.sl[k-1]
+            elif idx > len(self.sl) - k:
+                self.bad += num
+                self.bad -= self.sl[len(self.sl) - k]
+            self.sl.add(num)
+        else:
+            rem = self.q.popleft()
+            idx = self.sl.bisect_left(rem)
+            if idx < k:
+                self.bad -= rem
+                self.bad += self.sl[k]
+            elif idx >= len(self.sl) - k:
+                self.bad -= rem
+                self.bad += self.sl[len(self.sl) - k - 1]
+            self.sl.remove(rem)
+        
 
     def calculateMKAverage(self) -> int:
         print(self.tot, self.bad, self.sl)
